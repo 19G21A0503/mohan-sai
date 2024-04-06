2e9ad8b8cf1d2b837fabeab2b30e3a958e04170b
Mohan Sai Adavikayala <118970152+19G21A0503@users.noreply.github.com>
2024-04-06 12:55:41 +0530

    Add files via upload

---
+++ numsystem/decimal_flt.py
@@ -0,0 +1,41 @@
+
+#Float
+#Implicit
+a = 50.3
+print(a)
+print(type(a))
+
+#Explicit
+a = float(10.01)
+print(a)
+print(type(a))
+
+#Variable AAnnotation
+a: float = 1.3
+print(a)
+print(type(a))
+
+#Binary
+a = float(0b1010)
+print(a)
+print(type(a))
+
+#Octal
+a = float(0o1010)
+print(a)
+print(type(a))
+
+#HexaDecimal
+a = float(0x1010)
+print(a)
+print(type(a))
+
+#Boolean
+a = float(True)
+print(a)
+print(type(a))
+
+#2 Boolean
+a = float(False)
+print(a)
+print(type(a))
---
+++ numsystem/decimal_integer.py
@@ -0,0 +1,42 @@
+
+#Decimal System
+#integer
+#Implicit
+a = 503
+print(a)
+print(type(a))
+
+#Explicit
+a = int(3)
+print(a)
+print(type(a))
+
+#Variable Annotation
+a: int = 14
+print(a)
+print(type(a))
+
+#Binary Formate
+a = bin(3)
+print(a)
+print(type(a))
+
+#Octal
+a = oct(2)
+print(a)
+print(type(a))
+
+#HexaDecimal
+a = hex(4)
+print(a)
+print(type(a))
+
+#Boolean
+a = int(True)
+print(a)
+print(type(a))
+
+#2 Example
+a = int(False)
+print(a)
+print(type(a))
